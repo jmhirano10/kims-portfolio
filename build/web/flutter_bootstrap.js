c25f0a70ab1049aa1c9bca4c40027734ec7e443c
jmhirano10 <jmhirano@waterloo.ca>
2026-02-10 00:04:20 -0500

    Update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3832343531"
+    serviceWorkerVersion: "866960227"
   }
 });
