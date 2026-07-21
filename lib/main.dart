13314a441ca32a9dd6d0aba7e5930ee19ffb315e
jmhirano10 <jmhirano@waterloo.ca>
2026-07-20 23:13:56 -0400

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:kims_portfolio/layout.dart';
 import 'package:kims_portfolio/pages/portfolio.dart';
  
 void main() {
